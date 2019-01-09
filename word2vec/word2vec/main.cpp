7ef77b77afce993ca4322a19f289061685e19f9e
nancy <nancy_gu_87630012@qq.com>
2019-01-09 21:14:01 +0800

    add project 2019-1-9

---
+++ word2vec/word2vec/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  word2vec
+//
+//  Created by 顾楠希 on 2019/1/9.
+//  Copyright © 2019 顾楠希. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
